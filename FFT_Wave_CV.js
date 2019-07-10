21e583513746605113c5b4d3da6644192ed00990
Jules Kho <39061516+JewelsCo18@users.noreply.github.com>
2019-07-10 14:36:19 -0400

    pause, ipad, rbg bar

--- FFT_Wave_CV.js
+++ FFT_Wave_CV.js
@@ -1,10 +1,13 @@
+//IPAD SCREEN SIZE 
+//createCanvas(980, 800); 
+
 var mic, fft, cnv, input, points, backgroundColorPicker;
 
-//Spectrum
+//Spectrum Vars
 var divisions = 5;
 var speed = 1;
 
-//Mic 
+//Mic Vars
 var top_zero = false; 
 var micOn = false;
 var buttons = [];
@@ -12,6 +15,11 @@ var buttonState = [];
 var soundFile = [];
 var NumButtons = 5;
 var wave = [];
+var mic = new p5.AudioIn(); 
+var paused = 0; 
+
+//Test Variables
+var scaling = 1;
 
 
 //Global Setup for Bottom FFT (Landscape Frequency)
@@ -19,10 +27,10 @@ function setup() {
   //for safari use of microphone
   userStartAudio();
 
-  cnv = createCanvas(windowWidth/1.2, windowHeight/2);
-  cnv.position((windowWidth/6)+10,windowHeight/2); 
+  //cnv = createCanvas(windowWidth/1.2, windowHeight/2);
+  cnv = createCanvas(980 - (139), 400); 
+  cnv.position(220, 400); 
 
-  mic = new p5.AudioIn();
   mic.start();
 
   fft = new p5.FFT(0.8, 1024);
@@ -33,15 +41,26 @@ function setup() {
 
   // connect the mic to the recorder
   recorder.setInput(mic);
-}
+
+//   test_button = createButton("press");
+//   test_button.position(100,50); 
+//   test_button.mousePressed(clickEvent); 
+  }
+
+// function clickEvent() { 
+  
+// }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 function draw() {
   noFill();
 
+  //Zoom function??? 
+  //scaling = mouseX/100; 
+
   //variable to pinpoint correct heights of sound
-  var h = (height/divisions);
+  var h = height/divisions;
   var spectrum = fft.analyze();
   var newBuffer = [];
   var scaledSpectrum = splitOctaves(spectrum, 12);
@@ -49,35 +68,44 @@ function draw() {
   
   //aesthetics
   stroke(rline_slide.value(),gline_slide.value(),bline_slide.value(),100);
-  background(r_slide.value(),g_slide.value(),b_slide.value(),1);
+  background(red_slide.value(),green_slide.value(),blue_slide.value(),1);
 
   // copy before clearing the background
   copy(cnv,0,0,width,height,0,speed,width,height);
 
-  // draw shape
-  beginShape();
+  if (paused == 0) {
+    speed = 1; 
+    //draw shape
+    beginShape();
 
-    // one at the far corner
-    curveVertex(0, h);
+      // one at the far corner
+      curveVertex(0, h);
 
-    for (var i = 0; i < len; i++) {
-      var point = smoothPoint(scaledSpectrum, i, 2);
-      var x = map(i, 0, len-1, 0, width);
-      var y = map(point, 0, 255, h+150, 0);
-      curveVertex(x, y);
-    }
-    curveVertex(width, h);
-  endShape();
+      for (var i = 0; i < len; i++) {
+        var point = smoothPoint(scaledSpectrum, i, 2);
+        var x = map(i, 0, len-1, 0, width);
+        var y = map(point, 0, 255, h+150, 0);
+        curveVertex(x, y);
+      }
+
+      // one last point at the end
+      curveVertex(width, h);
+    endShape();
+  }
+  else{
+    speed = 0;
+  }
 }
 
-function splitOctaves(spectrum, slicesPerOctave) {
   
+function splitOctaves(spectrum, slicesPerOctave) {
   fft.analyze(); 
 
   var scaledSpectrum = [];
   var len = spectrum.length;
 
-  //optional adjustment of spectrum points dependent of frequency ranges
+  // default to thirds
+    //optional adjustment of spectrum points dependent of frequency ranges
   if (top_zero == false) {
     if (fft.getEnergy("treble") == true) {
       points = trebleslider.value();
@@ -105,8 +133,7 @@ function splitOctaves(spectrum, slicesPerOctave) {
 
   //print("array", trebleslider.value(), bassslider.value(), midslider.value()); 
   //print(points);
-  
-  var nthRootOfTwo = Math.pow(2, 1/points);
+  var nthRootOfTwo = Math.pow(2, 1/ points);
 
   // the last N bins get their own 
   var lowestBin = slicesPerOctave;
@@ -144,7 +171,6 @@ function splitOctaves(spectrum, slicesPerOctave) {
 
   // reverse so that array has same order as original array (low to high frequencies)
   scaledSpectrum.reverse();
-
   return scaledSpectrum;
 }
 
@@ -155,9 +181,7 @@ function smoothPoint(spectrum, index, numberOfNeighbors) {
   // default to 2 neighbors on either side
   var neighbors = numberOfNeighbors || 2;
   var len = spectrum.length;
-
   var val = 0;
-
   // start below the index
   var indexMinusNeighbors = index - neighbors;
   var smoothedPoints = 0;
@@ -169,9 +193,7 @@ function smoothPoint(spectrum, index, numberOfNeighbors) {
       smoothedPoints++;
     }
   }
-
   val = val/smoothedPoints;
-
   return val;
 }
 
@@ -180,13 +202,14 @@ function smoothPoint(spectrum, index, numberOfNeighbors) {
 //Where all descriptions and text go
 var side_bar = function(p) { 
   var side_cnv; 
-  var header_x = 10; 
-  var descriptor_x = 150;
+  var header_x = 10;
+  var slide_x = 15;  
+  var descriptor_x = 160;
 
   p.setup = function() {
-    side_cnv = p.createCanvas(windowWidth/6, windowHeight); 
+    side_cnv = p.createCanvas(220, 800); 
     side_cnv.position(0,0);
-    p.background(49,51,53, 100);
+    p.background(192,192,192,100);
     p.noLoop();
 
     micButton = createDiv('Mic ON');
@@ -209,33 +232,66 @@ var side_bar = function(p) {
 
     //Frequency Sliders
     bassslider = createSlider(0,15,3); 
-    bassslider.position(header_x, 430); 
+    bassslider.position(slide_x, 430); 
 
     midslider = createSlider(0,15,3); 
-    midslider.position(header_x, 450); 
+    midslider.position(slide_x, 450); 
 
     trebleslider = createSlider(0,15,12); 
-    trebleslider.position(header_x, 470); 
+    trebleslider.position(slide_x, 470); 
 
     //Stroke Sliders
+    rline_background = createDiv("red"); 
+    rline_background.class("red_line_bar"); 
+    rline_background.position(header_x, 580); 
+
     rline_slide = createSlider(0, 255, 255); 
-    rline_slide.position(header_x, 580); 
+    rline_slide.position(slide_x, 580); 
+
+    //////
+
+    gline_background = createDiv("green"); 
+    gline_background.class("green_line_bar"); 
+    gline_background.position(header_x, 610);
 
     gline_slide = createSlider(0,255, 119); 
-    gline_slide.position(header_x, 600); 
+    gline_slide.position(slide_x, 610); 
+
+    ////
+
+    bline_background = createDiv("blue"); 
+    bline_background.class("blue_line_bar"); 
+    bline_background.position(header_x, 640);
 
     bline_slide = createSlider(0,255,0); 
-    bline_slide.position(header_x, 620); 
+    bline_slide.position(slide_x, 640); 
 
     //Background Sliders
-    r_slide = createSlider(0, 255, 255); 
-    r_slide.position(header_x, 670); 
 
-    g_slide = createSlider(0,255, 255); 
-    g_slide.position(header_x, 690); 
+    red_background = createDiv("red"); 
+    red_background.class("red_back_bar"); 
+    red_background.position(header_x, 690); 
 
-    b_slide = createSlider(0,255,255); 
-    b_slide.position(header_x, 710); 
+    red_slide = createSlider(0, 255, 255); 
+    red_slide.position(slide_x, 690); 
+
+    /////////
+
+    green_background = createDiv("green"); 
+    green_background.class("green_back_bar"); 
+    green_background.position(header_x, 720)
+
+    green_slide = createSlider(0,255, 255); 
+    green_slide.position(slide_x, 720); 
+
+    /////////
+
+    blue_background = createDiv("blue"); 
+    blue_background.class("blue_back_bar"); 
+    blue_background.position(header_x, 750)
+
+    blue_slide = createSlider(0,255,255); 
+    blue_slide.position(slide_x, 750); 
   }
 
   p.draw = function() {
@@ -256,27 +312,27 @@ var side_bar = function(p) {
     p.textSize(14);
 
     //Frequency Blurb
-    p.text("Slide through values 0 to 15 to change", header_x,400);
-    p.text("input sensitivity for each frequency range!", header_x, 415);
+    p.text("Slide through values 0 to 15", header_x,400);
+    p.text("to change frequency range input", header_x, 415);
     
     //frequency slider descriptions
-    p.text("Bass input", descriptor_x, 442); 
-    p.text("Mid input", descriptor_x, 462);  
-    p.text("Treble input", descriptor_x, 482);
+    p.text("Bass", descriptor_x, 442); 
+    p.text("Mid", descriptor_x, 462);  
+    p.text("Treble", descriptor_x, 482);
 
     //colour picker description
-    p.text("Change the line and background color", header_x, 545); 
+    p.text("Change the line/background color", header_x, 547); 
 
     // //colour slider descriptions
     p.text("Line Color", header_x, 570); 
     p.text("Red", descriptor_x, 594); 
-    p.text("Green", descriptor_x, 614); 
-    p.text("Blue", descriptor_x, 634); 
+    p.text("Green", descriptor_x, 624); 
+    p.text("Blue", descriptor_x, 654); 
 
-    p.text("Background Color", header_x, 660); 
-    p.text("Red", descriptor_x, 684); 
-    p.text("Green", descriptor_x, 704); 
-    p.text("Blue", descriptor_x, 724); 
+    p.text("Background Color", header_x, 680); 
+    p.text("Red", descriptor_x, 704); 
+    p.text("Green", descriptor_x, 734); 
+    p.text("Blue", descriptor_x, 764); 
 
   }
 
@@ -288,43 +344,37 @@ var side_bar = function(p) {
 var o_sketch = function(p) { 
   p.x = 100; 
   p.y = 100;  
-  p.mic; 
   p.fft; 
   p.trigger;
   p.working = true; 
   var o_cnv; 
 
   p.setup = function() {
-    o_cnv = p.createCanvas(windowWidth/1.2, windowHeight/2);
-    o_cnv.position(250,0); 
+    o_cnv = p.createCanvas(980 - (139), 400);
+    o_cnv.position(220,0); 
     
-    p.mic = new p5.AudioIn();
     p.fft = new p5.FFT(0.8, 2048);
 
-    p.mic.start(); 
-    p.fft.setInput(p.mic); 
+    p.fft.setInput(mic); 
 
   }
   p.draw = function() {
-    p.strokeWeight(2);
-    p.noFill();
+    if (paused==0) {
+      p.strokeWeight(2);
+      p.noFill();
 
-    p.stroke(rline_slide.value(),gline_slide.value(),bline_slide.value());
-    p.background(r_slide.value(),g_slide.value(),b_slide.value());
+      p.stroke(rline_slide.value(),gline_slide.value(),bline_slide.value());
+      p.background(red_slide.value(),green_slide.value(),blue_slide.value());
 
-    //Dr. Kim's code don't know where to utilise
-    // var spectrum = p.fft.analyze();
-    // var subbands = p.fft.getOctaveBands(12,55.0);
-    // var energy = p.fft.logAverages(subbands);
+      var wave = p.fft.waveform();
 
-    var wave = p.fft.waveform();
-  
-    p.beginShape();
-    for (i = 0; i < wave.length; i++) {
-      vertex(i, map(wave[i], 0, 1, 200, 100));
-      }
-      p.endShape();
-    }
+      p.beginShape();
+      for (i = 0; i < wave.length; i++) {
+        vertex(i, map(wave[i]*scaling, 0, 1, 200, 100));
+        }
+        p.endShape();
+      }  
+    }  
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -362,7 +412,7 @@ function toggleButton(idx) {
       mic.stop();
       micOn = false;
       micButton.style('background-color','#888888');
-
+      micButton.html("Mic OFF"); 
       fft.setInput(soundFile[idx]);
       soundFile[idx].play(); // play the result!    
     }
@@ -374,13 +424,14 @@ function toggleButton(idx) {
 //Dr Kim's audio toggle
 function restartMic() {
   if ( !micOn ) {
+    paused = 0;
     mic.start();
     micOn = true;
     micButton.html('Mic ON');
     micButton.style('background-color', '#4400ff');
   }
   else {
-    mic.stop();
+    paused = 1; 
     micOn = false;
     micButton.html('Mic OFF');
     micButton.style('background-color', '#888888');
@@ -389,12 +440,11 @@ function restartMic() {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-function windowResized() {
-  resizeCanvas(windowWidth/1.2, windowHeight/2);
-  cnv.position(250, windowHeight/2); 
-  o_p5.resizeCanvas(windowWidth/1.2, windowHeight/2);
-  background(255,255,255,1); 
-  o_p5.background(0,0,0);
-  o_sketch.position(250, windowHeight/2); 
-}
+//RESIZE FUNCTION FOR WEB 
+// function windowResized() {
+//   resizeCanvas(980 - (windowWidth/6 +10), 400);
+//   cnv.position(windowWidth/6-20, windowHeight/2); 
+//   o_p5.resizeCanvas(980 - (windowWidth/6 +10), 400);
+//   o_sketch.position(windowWidth/6-20, windowHeight/2); 
+// }
 
